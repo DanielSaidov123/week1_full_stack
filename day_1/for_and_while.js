0c581f5415a06dab348413d88de1e43aea111941
DanielSaidov123 <ddny3622@gmail.com>
2025-11-30 16:35:37 +0200

    add  to day 1 file for loop

---
+++ day_1/for_and_while.js
@@ -0,0 +1,430 @@
+arr=[1,23,3,4,5]
+num=0
+while(num < arr.length){
+    console.log(arr[num]);
+    num+=1
+}
+console.log('//////////');
+
+
+let number=5
+while(number>0){
+    console.log(number);
+    number-=1
+    
+ }
+
+console.log('//////////');
+
+
+let num2=1
+let sum=0
+while(num2<=10){
+    sum+=num2
+    num2+=1
+ }
+console.log(sum);
+
+console.log('////////////////');
+
+let num3=2
+while(num3 <=10){ 
+    console.log(num3);
+    num3+=2
+    
+}
+console.log('///////////////');
+
+let num4 =3
+while(num4<=10){
+    console.log(num4);
+    num4+=2
+    
+ }
+
+console.log('/////////////');
+
+let num5=1
+while(num5<=3){
+    console.log(num5);
+    num5+=1
+ }
+console.log('////////////////');
+
+let num6=5
+while(num6<=25){
+    console.log(num6);
+    num6+=5
+ }
+
+console.log('//////////////');
+
+
+let num7=10
+while(num7>0){
+    console.log(num7);
+    num7-=2
+    
+ }
+console.log('////////////////');
+
+
+
+let num8=0
+while(num8<=3){
+    console.log('hello');
+    num8+=1
+    
+ }
+
+console.log('///////////');
+
+let num9=2
+while(num9<=10){
+    console.log(num9);
+    num9+=2
+ }
+console.log('/////////////////////@@@@@@@@@/////////////////////');
+
+for (let i = 1; i <= 5;i++){
+    console.log(i);
+    
+}
+console.log('/////////////////');
+
+for (let i = 5; i >0 ; i--) {
+    console.log(i);
+    
+ }
+console.log('///////////////////');
+
+
+let sum1=0
+for (let i = 0; i <=10 ; i++) {
+    sum1+=i
+ }
+console.log(sum1);
+
+console.log('//////////////');
+
+for (let i = 2; i <=10 ; i+=2) { 
+    console.log(i);
+    
+}
+console.log('/////////////////');
+
+for (let i = 0; i <3 ; i++) {
+    console.log('JS');
+    
+ }
+
+ console.log('//////////////');
+ 
+for (let i = 1; i <=4 ; i++) {
+    console.log(i);
+    
+ }
+
+console.log('/////////////');
+
+for (let i = 3; i <=15 ; i+=3) {
+    console.log(i);
+    
+ }
+console.log('///////////////');
+
+for (let i = 10; i >=0 ; i-=2) {
+    console.log(i);
+ }
+console.log('//////////////////');
+
+let sum2=0
+for (let i = 5; i <=15 ; i++) {
+    sum2+=i
+    
+ }
+console.log(sum2);
+
+console.log('//////////////////');
+
+for (let i = 1; i <4 ; i++) {
+    console.log(i,i**i);
+    
+ }
+console.log('/////////////////////');
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+    
+ }
+console.log('//////////////////');
+
+for (let i = 10; i >0 ; i--) { 
+    console.log(i);
+    
+}
+
+console.log('////////////////');
+
+for (let i = 2; i <=10 ; i+=2) {
+    console.log(i);
+    
+ }
+
+console.log('/////////////////////');
+
+for (let i = 5; i <= 15 ;i++) {
+    console.log('number:',i);
+    
+ }
+console.log('////////////////////');
+
+for (let i = 3; i <12 ; i+=3) {
+    console.log(i*2);
+    
+ }
+console.log('////////////////////');
+
+for (let i = 0; i <4 ; i++) {
+    console.log('hello');
+    
+ }
+console.log('/////////////////////');
+
+for (let i = 1; i <=3 ; i++) {
+    console.log('i is:',i);
+    
+ }
+console.log('/////////////////////');
+for (let i = 10; i >=0 ; i-=2) {
+    console.log(i);
+    
+ } 
+console.log('//////////////////////');
+let sum3=0
+for (let i = 1; i <=5 ; i++) {
+    sum3+=i    
+ }
+console.log(sum3);
+
+console.log('//////////////////////');
+for (let i = 5; i <10 ; i++) {
+    console.log(i+1);
+    
+ }
+
+console.log('//////////////////');
+
+let i=1
+while(i<=5){
+    console.log(i);
+        
+    i++ 
+}
+
+console.log('///////////////////');
+
+let i1 =10
+while(i1>=1){
+    console.log(i1);     
+    i1-- 
+}
+console.log('/////////////');
+
+let i3 = 2
+while(i3 <= 10){    
+    console.log(i3);
+    i3+=2 
+
+}
+console.log('///////////////');
+
+let i4=0 
+while(i4<3){ 
+
+    console.log('Hi');
+    i4++
+    
+ }
+console.log('//////////////////');
+
+ let i5 =5
+ while(i5<=15){
+    console.log('i5 is:',i5);
+    i5++
+    
+  }
+
+
+console.log('///////////////////');
+
+let i6 =10
+while(i>=0){
+  console.log(i);
+  
+  i-=2  
+ }
+console.log('/////////////////');
+
+let i7 =1
+while(i7<=4){
+    console.log(i7*i7);
+    
+    i7++
+ }
+
+console.log('////////////////');
+let i8=3
+while(i8<12){
+    console.log(i8);
+    i8+=3
+    
+ }
+console.log('/////////////////////');
+let i9=1
+while(i9<=3){
+    console.log('Loop:',i9);
+    i9++
+ }
+
+console.log('/////////////////');
+
+let i10 = 1;
+while ( i10<= 5) {
+console.log(i10);
+i10++
+}
+
+console.log('///////////////////');
+
+let i11 = 10;
+while (i11 >= 0) {
+console.log(i11);
+i11--; // wrong direction
+}
+console.log('////////////////////');
+
+let i12 = 0;
+while (i12 < 3) {
+console.log("hello");
+i12++
+}
+
+console.log('/////////////////////');
+
+let i13 = 5;
+while (i13 > 0) {
+console.log(i13);
+i13--
+}
+console.log('///////////////////');
+
+let i14 = 1;
+while (i14 !== 10) {
+console.log(i14);
+i14 += 1;
+}
+console.log('///////////////////');
+
+let i15 = 2;
+while (i15 < 20) {
+console.log(i15);
+i15 = i15 + 2;
+}
+console.log('////////////////////');
+
+let i16 = 0;
+while (i16 <= 4) {
+console.log(i16);
+i16++
+}
+console.log('////////////////////////');
+
+let i17 = 10;
+while (i17 >= 0) {
+console.log(i17);
+ i17--
+}
+console.log('////////////////////////');
+
+let i18 = 1;
+while (i18 < 5) {
+console.log(i18);
+i18 = i18 + 1; // useless change
+}
+
+console.log('//////////////////////////');
+let i19 = 3;
+while (i19 > 0) {
+console.log(i19);
+i19 = i19 - 2;
+}
+console.log('////////////////////////////');
+
+for (let i = 1; i <= 5;i++ ) {
+console.log(i);
+}
+console.log('////////////////////////////////');
+
+for (let i = 10; i >= 0; i--) {
+console.log(i);
+}
+console.log('////////////////////');
+
+for (let i = 0; i < 3;i++ ) {
+console.log("hi");
+}
+console.log('//////////////////////');
+
+for (let i = 5; i > 0; i--) {
+console.log(i);
+}
+console.log('/////////////////////////');
+for (let i = 1; i !== 10; i++) {
+console.log(i);
+}
+console.log('/////////////////////////////');
+for (let i = 2; i < 20; i += 2) {
+console.log(i);
+}
+
+console.log('////////////////////');
+
+for (let i = 0; i <= 4;i++ ) {
+console.log(i);
+}
+
+console.log('//////////////////////');
+for (let i = 10; i >= 0;i-- ) {
+console.log(i);
+}
+
+console.log('///////////////////////');
+for (let i = 3; i > 0;i-=2 ) {
+console.log(i);
+}
+console.log('//////////////////////////');
+
+for (let i = 1; i < 5;  i++) {
+console.log(i);
+}
+
+console.log('/////////////////');
+let j = 1;
+while (j <= 3) {
+console.log(j);
+j++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
