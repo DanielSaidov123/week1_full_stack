2d2a352a3bcdb9f82e9fc3a41c6b7f4e142cc59a
DanielSaidov123 <ddny3622@gmail.com>
2025-12-02 17:39:24 +0200

    add

--- day_3/functions_and_error_handling.js
+++ day_3/functions_and_error_handling.js
@@ -25,8 +25,42 @@ return total
 }
 
 console. log (sumAges ("10 20 30 "));
+console.log('------------------');
 
-// function extractNumbers(arr){
-//     if(arr==='list'){ };
+function extractNumbers(arr){
+    if(Array.isArray(arr)){
+    const arr2=[]
+     for (let i = 0; i <arr.length ; i++) {
+        if(typeof arr[i]==='number'){
+            arr2.push(arr[i])
+         };
 
-//  } 
+      }
+      return arr2
+    }
+    else{
+        return 'erorr'
+    }
+     
+ } 
+ 
+ console.log(extractNumbers([1,2,3]));
+
+
+ console.log('------------------');
+
+function sumNumbersSafe(arr){ 
+    const arr2=extractNumbers(arr)
+    if(Array.isArray(arr2)){
+    return arr2.reduce((a,b)=>a+b,0)
+    }
+    else{
+        return 'erorr'
+    }
+} 
+
+console.log(sumNumbersSafe( [1,2,3,'d',4]));
+
+
+
+ 
