cd9acc9ba017faaa1950e79eb13262d19dbed4f8
DanielSaidov123 <ddny3622@gmail.com>
2025-12-03 17:56:21 +0200

    add padetc

--- project_1/client/io.js
+++ project_1/client/io.js
@@ -1,5 +1,6 @@
 import func from "../utils/util.js"
 import readlineSync from 'readline-sync';
+import input from 'analiza-sync';
  
 
 
@@ -12,12 +13,12 @@ while(true){
 5. Edit report by ID
 0. Exit
 `)
-var number = readlineSync.question('enter a namber: ');
+var number = input('enter a namber: ');
 if(number=='1'){
-    let id = readlineSync.question('enter a id: ');
-    let name = readlineSync.question('enter a name: ');
-    let weapons = readlineSync.question('enter a weapons: ');
-    let text = readlineSync.question('enter a text: ');
+    let id = input('enter a id: ');
+    let name = input('enter a name: ');
+    let weapons = input('enter a weapons: ');
+    let text = input('enter a text: ');
     const obj=func.create_report(id,name,weapons,text)
     console.log(func.add_report_to_data(obj));
  }; 
@@ -26,25 +27,25 @@ if(number=='1'){
   };
 
 if(number=='3'){
-    let id = readlineSync.question('enter a id: ');
+    let id = input('enter a id: ');
     console.log(func.searching_report_by_ID(id));
     
     
  };
  if(number=='4'){
-    let id = readlineSync.question('enter a id: ');
+    let id = input('enter a id: ');
     console.log(func.default_report_by_ID(id));
   };
   if(number=='5'){
-    let id = readlineSync.question('enter a id: ');
-    let name = readlineSync.question('enter a name: ');
-    let weapons = readlineSync.question('enter a weapons: ');
-    let report_text = readlineSync.question('enter a text: ');
+    let id = input('enter a id: ');
+    let name = input('enter a name: ');
+    let weapons = input('enter a weapons: ');
+    let report_text = input('enter a text: ');
     const ob={name:name,weapons:weapons,report_text:report_text}
     console.log(func.existing_report(id,ob));
     
    };
-   if(number=='6'){
+   if(number=='0'){
     break
     };
  }
